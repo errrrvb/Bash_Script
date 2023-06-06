0edb799095bf0a58f634abbb37c4033485e9007b
errrrvb <roman-vavilov123region@yandex.ru>
2023-06-06 19:17:26 +0300

    done

--- filetype_test.sh
+++ filetype_test.sh
@@ -5,6 +5,8 @@ output=$(bash script_txt.sh something.exe)
 expected_output=""
 if [[ "$output" == "$expected_output" ]]; then
     echo "Тест 2 пройден"
+    exit 0
 else
     echo "Тест 2 не пройден"
+    exit 1
 fi
